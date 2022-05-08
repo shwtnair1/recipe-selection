3eeb2cd4526d7815fe272c0786ef522bf48d9bb1
shwtnair1 <shwt.nair1@gmail.com>
2022-05-08 20:17:01 +0530

    Handle UI for cases where props are not passed

--- src/modules/recipes/PriceSummary.js
+++ src/modules/recipes/PriceSummary.js
@@ -11,20 +11,22 @@ let hrStyle={
 };
 // Create PriceSummary user interface
 const PriceSummary = ({ summary, totalPrice }) => {
+  const shippingPrice = summary && summary.shippingPrice ? summary.shippingPrice : 0;
+  const items = summary && summary.items? summary.items : []; 
   return(
   <Box width={['290px', '450px']} padding='16px'>
-      {summary.items.map((item)=>{
-        return<Flex key={item.id} justifyContent='space-between' marginBottom='8px' fontSize='16px' lineHeight='27px'>
-            <Text fontWeight={400}>{item.name} {item.selected > 1 ? 'x '+item.selected:''}</Text>
+      {items.length >0 && items.map((item)=>{
+        return<Flex data-testid="item-list" key={item.id} justifyContent='space-between' marginBottom='8px' fontSize='16px' lineHeight='27px'>
+            <Text fontWeight={400}>{item.name}{item.selected > 1 ? ' x '+item.selected:''}</Text>
             <Text>{parseRawPrice(item.price)}</Text>
           </Flex>
       })}
-      <Flex justifyContent='space-between' marginTop='8px' fontSize='16px' lineHeight='27px'>
+      <Flex data-testid="shipping-price" justifyContent='space-between' marginTop='8px' fontSize='16px' lineHeight='27px'>
             <Text fontWeight={400} >Shipping</Text>
-            <Text>{parseRawPrice(summary.shippingPrice)}</Text>
+            <Text>{parseRawPrice(shippingPrice)}</Text>
       </Flex>
       <hr style={hrStyle}/>
-      <Flex justifyContent='space-between' marginTop='8px' fontSize='16px' lineHeight='27px'>
+      <Flex data-testid="total-price" justifyContent='space-between' marginTop='8px' fontSize='16px' lineHeight='27px'>
             <Text fontWeight={600}>Total</Text>
             <Text fontWeight={600}>{totalPrice}</Text>
       </Flex>
